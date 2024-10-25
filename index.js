198590b15be25c8a6770e66220b0cf6b6c47bb23
Praveen Kumar <praveenganesh7@gmail.com>
2024-10-25 09:22:44 +0530

    update: change default port no

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express();
 const routes = require('./routes');
 
-const PORT = process.env.PORTNO || 5000;
+const PORT = process.env.PORTNO || 5555;
 
 app.use(routes);
 
